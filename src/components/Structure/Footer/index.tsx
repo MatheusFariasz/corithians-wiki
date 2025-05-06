440ce019d2050311b534fe0d20e6991dd10d8b5a
Matheus Farias <matheusg.farias@hotmail.com>
2025-05-06 13:54:18 -0300

    feat: creating the footer

---
+++ src/components/Structure/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { FooterWrapper, Anchor } from "./styles";
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      Sport Club Corinthians Paulista — Desenvolvido por{" "}
+      <Anchor href="https://github.com/MatheusFariasz" target="_blank">
+        Matheus Farias
+      </Anchor>
+      .
+    </FooterWrapper>
+  );
+}
