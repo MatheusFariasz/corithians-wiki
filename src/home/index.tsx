e052094fe7b863639a3cded5324f257ca855861c
Matheus Farias <matheusg.farias@hotmail.com>
2025-05-06 14:02:08 -0300

    feat: creating home page

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,12 +1,35 @@
-import { Main } from "./styles";
-import Teste from "../components/Players";
-import Titles from "../components/Titles"
+import {
+  Container,
+  Logo,
+  LeftSection,
+  RightSection,
+  Title,
+  Nav,
+  StyledLink,
+  Header,
+  NavSection,
+} from "./styles";
 
 export default function Home() {
   return (
-    <Main>
-      <Teste />
-      <Titles />
-    </Main>
+    <Container>
+      <LeftSection>
+        <Logo
+          src="../../public/assets/icons/corinthians.svg"
+          alt="Corinthians Logo"
+        />
+      </LeftSection>
+      <RightSection>
+        <Header>
+          <Title>MENU</Title>
+        </Header>
+        <NavSection>
+          <Nav>
+            <StyledLink to="/players">Ver Jogadores</StyledLink>
+            <StyledLink to="/titles">Ver Títulos</StyledLink>
+          </Nav>
+        </NavSection>
+      </RightSection>
+    </Container>
   );
 }
